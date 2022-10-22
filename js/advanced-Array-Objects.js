72d021f39e089e33405cb9d2b270174c14027f28
David Lara <david.a.lara05@gmail.com>
2022-10-22 17:39:12 -0500

    using .length

--- js/advanced-Array-Objects.js
+++ js/advanced-Array-Objects.js
@@ -1,12 +1,44 @@
 
 
-const howManyLetters = () => {
-    const phrase = "hey, can you go grocery store with me?"
+// Looops through the letters in phrase
+// const howManyLetters = () => {
+//     const phrase = "hey, can you go to the grocery store with me?"
 
-    for (letter in phrase) {
-        console.log(letter);
-    }
+//     let result = 0;
 
+//     for (const index in phrase) {
+//         console.log(Number(index)+ 1);
+//         result = Number(index)+ 1
+//     }
+//     return {result} // by putting on curly brackets it returns results: 37
+
+// }
+
+// console.log(howManyLetters());
+
+
+// This one prompt the user and counts the letters on their phrase 
+
+// const howManyLetters = () => {
+    
+//    let result = 0;
+
+//     for (const index in phrase) {
+//         console.log(Number(index)+ 1);
+//         result = Number(index)+ 1
+//     }
+//     return {result} // by putting on curly brackets it returns results: 37
+
+// }
+// const phrase = prompt('Write your phrase');
+// console.log(howManyLetters(phrase));
+
+// Using .length to count the letters 
+
+const howManyLetters = (phrase) => {
+    return { result: phrase.length }
 }
 
-console.log(howManyLetters());
+const phrase = prompt( "Write your phase")
+
+console.log(howManyLetters(phrase))
