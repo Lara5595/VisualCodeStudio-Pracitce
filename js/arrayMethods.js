fdb07895336f43aa8522e81632e4bbcbc89fb899
David Lara <david.a.lara05@gmail.com>
2022-10-22 20:01:06 -0500

    working on .map

---
+++ js/arrayMethods.js
@@ -0,0 +1,26 @@
+// Higher order functions
+// map - loops and return an array
+// filter
+// reduce
+
+
+// This loop thorugh the numbers with map
+// [1, 2, 3, 4].map(number => console.log(number));
+
+
+// This loops and multiplies by 2
+// let result = [1, 2, 3, 4].map(number => number * 2)
+// console.log(result);
+
+
+// This is a function with map
+// const doubleMap = (numbers) => {
+//  return numbers.map(number => number * 2)
+// }
+
+// console.log(doubleMap([1, 2, 3]));
+
+function doubleMap(numbers) {
+    return numbers * 2
+}
+
