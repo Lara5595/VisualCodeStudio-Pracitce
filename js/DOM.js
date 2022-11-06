b70d047d7342ae985928e929cfdf0c46d73908bf
David Lara <david.a.lara05@gmail.com>
2022-11-06 12:03:40 -0600

    used template lit

--- js/DOM.js
+++ js/DOM.js
@@ -5,5 +5,15 @@ let title = document.getElementById("title")
 
 console.log(title.innerText);
 
-// This changes the text
-title.innerText = 'Goodbye my lover!'
+// This changes the text in html for the id of title
+// title.innerText = 'Goodbye my lover!'
+
+
+// Changing its html
+let message = 'Goodbye my lover!'
+title.innerText = message;
+// This change the text to hello and made it a paragraph
+title.innerHTML = '<p>Hello</p>'
+
+// replace the word hello with the message variable using template literals
+title.innerHTML = `<p>${message}</p>`
